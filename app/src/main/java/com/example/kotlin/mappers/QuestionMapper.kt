f4e62ec7f8bd92a05dc61ea450b38b4c586d8c13
facundograpsas <facundo.grapsas@gmail.com>
2023-09-17 22:24:49 -0300

    feat: add QuestionEntity and Mapper classes

---
+++ app/src/main/java/com/example/kotlin/mappers/QuestionMapper.kt
@@ -0,0 +1,45 @@
+package com.example.kotlin.mappers
+
+import com.example.kotlin.models.db.QuestionEntity
+import com.example.kotlin.models.network.Question
+import com.example.kotlin.models.network.TimeStamps
+
+object QuestionMapper {
+
+    fun toEntity(question: Question): QuestionEntity {
+        return QuestionEntity(
+            id = 0, // Room will auto-generate this for you
+            level = question.level,
+            levelInt = question.levelInt,
+            type = question.type,
+            text = question.text,
+            question = question.question,
+            options = question.options, // Assuming this is serializable to a string
+            correctAnswer = question.correctAnswer,
+            subQuestions = question.subQuestions,
+            correctAttempts = question.correctAttempts,
+            wrongAttempts = question.wrongAttempts,
+            createdAt = question.timestamps?.createdAt,
+            updatedAt = question.timestamps?.updatedAt
+        )
+    }
+
+    fun toModel(questionEntity: QuestionEntity): Question {
+        return Question(
+            level = questionEntity.level,
+            levelInt = questionEntity.levelInt,
+            type = questionEntity.type,
+            text = questionEntity.text,
+            question = questionEntity.question,
+            options = null,
+            correctAnswer = questionEntity.correctAnswer,
+            subQuestions = null,
+            correctAttempts = questionEntity.correctAttempts,
+            wrongAttempts = questionEntity.wrongAttempts,
+            timestamps = TimeStamps(
+                createdAt = questionEntity.createdAt ?: "",
+                updatedAt = questionEntity.updatedAt ?: ""
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/example/kotlin/models/db/QuestionEntity.kt
@@ -0,0 +1,80 @@
+package com.example.kotlin.models.db
+
+import android.util.Log
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.example.kotlin.models.network.Option
+import com.example.kotlin.models.network.SubQuestion
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+
+@Entity(tableName = "questions")
+data class QuestionEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0, // Auto-generate a unique ID
+    @ColumnInfo(name = "level") val level: String,
+    @ColumnInfo(name = "levelInt") val levelInt: Int,
+    @ColumnInfo(name = "type") val type: String,
+    @ColumnInfo(name = "text") val text: String,
+    @ColumnInfo(name = "question") val question: String?,
+
+    @TypeConverters(OptionConverter::class)
+    @ColumnInfo(name = "options") val options: List<Option>?,
+
+    @ColumnInfo(name = "correctAnswer") val correctAnswer: String,
+
+    @TypeConverters(SubQuestionConverter::class)
+    @ColumnInfo(name = "subQuestions") val subQuestions: List<SubQuestion>?,
+
+    @ColumnInfo(name = "correctAttempts") val correctAttempts: Int = 0,
+    @ColumnInfo(name = "wrongAttempts") val wrongAttempts: Int = 0,
+    @ColumnInfo(name = "createdAt") val createdAt: String?, // Assuming String format
+    @ColumnInfo(name = "updatedAt") val updatedAt: String?
+)
+
+class OptionConverter {
+
+    private val moshi = Moshi.Builder().build()
+    private val listType = Types.newParameterizedType(List::class.java, Option::class.java)
+
+    @TypeConverter
+    fun fromOptionList(options: List<Option>?): String {
+        Log.d("OptionConverterDebug", "Converting from Object: $options")
+        val json = moshi.adapter<List<Option>>(listType).toJson(options)
+        Log.d("OptionConverterDebug", "Converted to JSON: $json")
+        return json
+    }
+
+    @TypeConverter
+    fun toOptionList(optionString: String): List<Option>? {
+        Log.d("OptionConverterDebug", "Converting from JSON: $optionString")
+        val options = moshi.adapter<List<Option>>(listType).fromJson(optionString)
+        Log.d("OptionConverterDebug", "Converted to Object: $options")
+        return options
+    }
+}
+
+class SubQuestionConverter {
+
+    private val moshi = Moshi.Builder().build()
+    private val listType = Types.newParameterizedType(List::class.java, SubQuestion::class.java)
+    private val jsonAdapter: JsonAdapter<List<SubQuestion>> = moshi.adapter(listType)
+
+    @TypeConverter
+    fun subQuestionsToJson(subQuestions: List<SubQuestion>?): String {
+        val json = jsonAdapter.toJson(subQuestions)
+        Log.d("MoshiDebug", "Converted to JSON: $json")
+        return json
+    }
+
+    @TypeConverter
+    fun jsonToSubQuestions(json: String?): List<SubQuestion>? {
+        Log.d("MoshiDebug", "Converting from JSON: $json")
+        val subQuestions = jsonAdapter.fromJson(json.orEmpty())
+        Log.d("MoshiDebug", "Converted to Object: $subQuestions")
+        return subQuestions
+    }
+}
