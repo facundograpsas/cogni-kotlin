aa2142057be9f4e26aa61eb7badd98d8c828ef14
facundograpsas <facundo.grapsas@gmail.com>
2023-09-18 02:46:01 -0300

    feat: Add custom AppException class for error handling

---
+++ app/src/main/java/com/example/kotlin/utils/AppException.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin.utils
+
+sealed class AppException : Exception() {
+    object DatabaseError : AppException()
+    data class DatabaseInsertError(override val message: String) : AppException()
+    data class DatabaseReadingError(override val message: String) : AppException()
+    data class DatabaseEmptyError(override val message: String) : AppException()
+    data class NetworkError(override val message: String) : AppException()
+
+}
