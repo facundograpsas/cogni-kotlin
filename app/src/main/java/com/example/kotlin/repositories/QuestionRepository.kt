5d4cb14b48c0a7c81b93a9f2ddd698c0f265aeef
facundograpsas <facundo.grapsas@gmail.com>
2023-09-18 02:47:57 -0300

    feat: Add comprehensive error handling to QuestionRepository

--- app/src/main/java/com/example/kotlin/repositories/QuestionRepository.kt
+++ app/src/main/java/com/example/kotlin/repositories/QuestionRepository.kt
@@ -5,41 +5,49 @@ import com.example.kotlin.api.ApiClient
 import com.example.kotlin.database.QuestionDao
 import com.example.kotlin.models.db.QuestionEntity
 import com.example.kotlin.models.network.Question
+import com.example.kotlin.models.state.DataResult
+import com.example.kotlin.utils.AppException
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val questionDao: QuestionDao) {
 
-    suspend fun insertQuestion(question: QuestionEntity) {
-        return questionDao.insertQuestion(question)
-    }
-
-
-    suspend fun getQuestion(questionId: String): Question? {
+    suspend fun insertQuestions(question: List<QuestionEntity>): DataResult<Unit> {
         return try {
-            val response = ApiClient.questionService.getQuestion(questionId).execute()
-            if (response.isSuccessful) {
-                response.body()
-            } else {
-                null
-            }
+            questionDao.insertQuestions(question)
+            DataResult.Success(Unit)  // Indicates successful operation
         } catch (e: Exception) {
-            null
+            Log.e("DatabaseInsertError", "Error while inserting questions: ${e.message}", e)
+            DataResult.Failure(AppException.DatabaseInsertError("There's been an error trying to save the records in the database: ${e.message}"))
         }
     }
 
-    suspend fun getQuestions(): List<Question>? {
+    suspend fun getQuestionsFromLocalDb(): DataResult<List<QuestionEntity>> {
+        return try {
+            val questions = questionDao.getAllQuestions()
+            if (questions.isNotEmpty()) {
+                DataResult.Success(questions)
+            } else {
+                DataResult.Failure(AppException.DatabaseEmptyError("The database has no records."))
+            }
+        } catch (e: Exception) {
+            Log.e("DatabaseReadingError", "Error while reading questions: ${e.message}", e)
+            DataResult.Failure(AppException.DatabaseReadingError("There's been an error trying to read the records from the database: ${e.message}"))
+        }
+    }
+
+    suspend fun getQuestions(): DataResult<List<Question>> {
         return try {
             val response = ApiClient.questionService.getQuestions().execute()
             if (response.isSuccessful) {
                 response.body()?.let { body ->
-                    Log.i("info", "getQuestions: $body")
-                    return body
-                }
+                    return DataResult.Success(body)
+                } ?: DataResult.Failure(AppException.NetworkError("No body in the response. HTTP Code: ${response.code()}"))
+            } else {
+                DataResult.Failure(AppException.NetworkError("HTTP request failed. Code: ${response.code()}, Message: ${response.message()}"))
             }
-            null
         } catch (e: Exception) {
-            Log.e("error", "getQuestions failed: ${e.localizedMessage}")
-            null
+            Log.e("NetworkError", "Error while fetching questions: ${e.message}", e)
+            DataResult.Failure(AppException.NetworkError("There's been a network error: ${e.message}"))
         }
     }
 }
