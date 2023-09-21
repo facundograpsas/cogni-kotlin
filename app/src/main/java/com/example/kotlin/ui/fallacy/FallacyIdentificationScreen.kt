acfc8bc51e95267f5d1cce86a784006b1a530bf8
facundograpsas <facundo.grapsas@gmail.com>
2023-09-21 20:00:26 -0300

    feat: layout change on FallacyIdentificationScreen.kt

--- app/src/main/java/com/example/kotlin/ui/fallacy/FallacyIdentificationScreen.kt
+++ app/src/main/java/com/example/kotlin/ui/fallacy/FallacyIdentificationScreen.kt
@@ -1,18 +1,25 @@
 package com.example.kotlin.ui.fallacy
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -40,7 +47,7 @@ import com.example.kotlin.ui.FallacyScreenEvent
 
 
 @Composable
-fun FallacyIdentificationScreen(
+fun FallacyScoreScreen(
     currentQuestion : QuestionEntity,
     currentQuestionIndex : Int,
     questions : List<QuestionEntity>?,
@@ -111,41 +118,60 @@ fun FallacyIdentificationScreen(
                 .clip(RoundedCornerShape(20.dp)) // Rounded corners
                 .padding(top = 8.dp, bottom = 8.dp)
         )
-        Spacer(modifier = Modifier.weight(1f))
 
-        StyledCard(currentQuestion)
-        Spacer(modifier = Modifier.weight(1f))
 
-        currentQuestion.options!!.forEach { option ->
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Start,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable { selectedFallacy = option.value }
-            ) {
-                RadioButton(
-                    selected = selectedFallacy == option.value,
-                    onClick = { selectedFallacy = option.value }
-                )
-                Text(
-                    text = option.value,
-                    style = TextStyle(fontSize = 18.sp),
-                    modifier = Modifier.padding(start = 16.dp)
-                )
+        Column(
+            modifier = Modifier
+            .verticalScroll(rememberScrollState())
+                .weight(100f)
+
+        ) {
+            StyledCard(currentQuestion)
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+
+            currentQuestion.options!!.forEach { option ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { selectedFallacy = option.value }
+                ) {
+                    RadioButton(
+                        selected = selectedFallacy == option.value,
+                        onClick = { selectedFallacy = option.value }
+                    )
+                    Text(
+                        text = option.value,
+                        style = TextStyle(fontSize = 18.sp),
+                        modifier = Modifier.padding(start = 16.dp)
+                    )
+                }
             }
         }
 
-        Spacer(modifier = Modifier.weight(1f))
 
+        val buttonEnabled = selectedFallacy != null
+        val backgroundColor by animateColorAsState(
+            targetValue = if (buttonEnabled) Color.White else Color.Gray, label = ""
+        )
         Button(
+            enabled = selectedFallacy != null,
             onClick = {
-                onNextQuestion(selectedFallacy!!)
+                if(selectedFallacy!=null){
+                    onNextQuestion(selectedFallacy!!)
+                    selectedFallacy = null
+                }
             },
             modifier = Modifier
                 .fillMaxWidth()
-                .height(80.dp),
+                .weight(15f)
+            ,
             shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(containerColor  = backgroundColor)  // Use animated color here
+
 
         ) {
             Text("Next", style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
@@ -155,30 +181,36 @@ fun FallacyIdentificationScreen(
 
 
 @Composable
-fun StyledCard(currentQuestion: QuestionEntity) {
+fun ColumnScope.StyledCard(currentQuestion: QuestionEntity) {
     Card(
-        modifier = Modifier.fillMaxWidth()
+//        modifier = Modifier.fillMaxWidth().height(250.dp)
     ) {
         Column(
             modifier = Modifier
+                .fillMaxHeight()
+//                .verticalScroll(rememberScrollState())
                 .background(color = MaterialTheme.colorScheme.secondaryContainer)
                 .padding(16.dp)
         ) {
             Text(
                 text = currentQuestion.type,
+                color = MaterialTheme.colorScheme.onSecondary,
                 style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold)
             )
             Text(
                 text = currentQuestion.text,
+                color = MaterialTheme.colorScheme.onSecondary,
                 style = TextStyle(fontSize = 16.sp)
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = "${stringResource(id = R.string.question_string)}:",
+                color = MaterialTheme.colorScheme.onSecondary,
                 style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold)
             )
             Text(
                 text = currentQuestion.question!!,
+                color = MaterialTheme.colorScheme.onSecondary,
                 style = TextStyle(fontSize = 16.sp)
             )
         }
